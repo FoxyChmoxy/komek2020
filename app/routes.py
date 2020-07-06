e571969f65711b35b1114498e3a40ef23fd0acbb
Almas Zaurbekov <almasgaara@mail.ru>
2020-07-06 22:23:41 +0600

    global search is added

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,6 @@ from app import app
 from app.forms import NeedsForm, GiverForm
 from app.models import Komek
 from app import db
-# from functions import get_vacancies, get_vacancy_by_id
 
 @app.route('/')
 @app.route('/index')
@@ -18,7 +17,7 @@ def needs():
         city=form.city.data, service=form.needs.data, is_giver=False, flag=1)
         db.session.add(komek)
         db.session.commit()
-        return redirect(url_for('global_app', service=komek.service, city=komek.city))
+        return redirect(url_for('global_app', service=komek.service, city=komek.city, is_giver=True))
     return render_template('needs.html', title='Нужна помощь', form=form)
 
 @app.route('/giver', methods=['GET', 'POST'])
@@ -29,16 +28,25 @@ def giver():
         city=form.city.data, service=form.gift.data, is_giver=True, flag=1)
         db.session.add(komek)
         db.session.commit()
-        return redirect(url_for('global_app', service=komek.service, city=komek.city))
+        return redirect(url_for('global_app', service=komek.service, city=komek.city, is_giver=False))
     return render_template('giver.html', title='Могу помочь', form=form)
 
 @app.route('/global')
 def global_app():
-    service = str(request.args.get('service'))
-    city = str(request.args.get('city'))
-    is_giver = request.args.get('is_giver') == 'True'
+    service = request.args.get('service')
+    city = request.args.get('city')
+    is_giver = request.args.get('is_giver')
 
-    helps = Komek.query.filter(Komek.city==city, Komek.is_giver==(not is_giver))
-    helps = [help for help in helps if service in help.service]
-    return render_template('global.html', title='Глобальный поиск', helps=helps)
+    result = Komek.query.all()
+
+    if city is not None:
+        result = [obj for obj in result if obj.city == city.lower()]
+
+    if is_giver is not None:
+        result = [obj for obj in result if obj.is_giver == (is_giver == 'True')]
+
+    if service is not None:
+        result = [obj for obj in result if service in obj.service]
+    
+    return render_template('global.html', title='Глобальный поиск', helps=result)
     
